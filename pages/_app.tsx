0d8f12bc3c428b2ebe76a173dc40967daa47f0cd
Raphael Ornellas <raphaelso@live.com>
2023-12-21 21:14:02 -0300

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
-import type { AppProps } from 'next/app';
-import { SpeedInsights } from '@vercel/speed-insights/next';
- 
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
- 
+
 export default MyApp;
